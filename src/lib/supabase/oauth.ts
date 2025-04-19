2034e999f9a51dfdb6b40bf03c7f1f2831ec8173
binZuy <duynguyenminh0206@gmail.com>
2025-04-20 01:28:23 +0700

    Fix login

--- src/lib/supabase/oauth.ts
+++ src/lib/supabase/oauth.ts
@@ -7,13 +7,25 @@ import { createClient } from "./server"
 const signInWith = (provider:  Provider) => async () => {
     const supabase = await createClient();
 
-    const auth_callback_url = `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`;
-    console.log("auth_callback_url", auth_callback_url);
+    // const auth_callback_url = `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`;
+    // console.log("auth_callback_url", auth_callback_url);
+    const getURL = () => {
+        let url =
+          process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+          process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+          'http://localhost:3000/'
+        // Make sure to include `https://` when not localhost.
+        url = url.startsWith('http') ? url : `https://${url}`
+        // Make sure to include a trailing `/`.
+        url = url.endsWith('/') ? url : `${url}/`
+        return url
+      }
+      console.log("getURL", getURL())
     const { data, error } =
     await supabase.auth.signInWithOAuth({
         provider,
         options: {
-            redirectTo: auth_callback_url,
+            redirectTo: `${getURL()}auth/callback`,
         },
     });
 
